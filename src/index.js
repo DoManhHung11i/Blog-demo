0b3598afb7343ca9a9c540dfa777e30abc62abc3
DoManhHung11i <hung151004@gmail.com>
2024-03-31 01:30:19 +0700

    create route, app/controllers

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ const express = require('express');
 const morgan = require('morgan');
 const { engine } = require ('express-handlebars');
 const sass = require('sass');
+const { execPath } = require('process');
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
 
 app.use(morgan('combined'));
 
@@ -17,13 +24,8 @@ app.engine('hbs', engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-});
-
-app.get('/news', (req, res) => {
-  res.render('news');
-});
+//route init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
